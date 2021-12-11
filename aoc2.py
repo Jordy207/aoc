a2a4615a24fafdd777c4c886c9ba7b5963379589
Jordy de Wit <jordy1998@live.nl>
2021-12-11 20:23:57 +0100

    aoc2 part 2

---
+++ aoc2.py
@@ -0,0 +1,22 @@
+
+
+file1 = open('inputpuzzle2.txt', 'r')
+lines = file1.readlines()
+
+prev_line = float('inf')
+horizontal = 0
+depth = 0
+aim = 0
+
+for line in lines:
+    if line.split()[0] == 'forward':
+        horizontal += int(line.split()[1])
+        depth += int(line.split()[1]) * aim
+    if line.split()[0] == 'down':
+        # depth += int(line.split()[1])
+        aim += int(line.split()[1])
+    if line.split()[0] == 'up':
+        # depth += -int(line.split()[1])
+        aim += -int(line.split()[1])
+
+print(horizontal * depth)
