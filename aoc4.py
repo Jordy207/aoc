e983bf6cc7377f54edd62a70bab826f70ddde29a
Jordy de Wit <jordy1998@live.nl>
2021-12-12 01:20:37 +0100

    aoc4 part 2 finished

--- aoc4.py
+++ aoc4.py
@@ -37,24 +37,44 @@ for idx, line in enumerate(filelines[1:]):
         for idx1, entry in enumerate(line.split()):
             board_lines.append([int(entry), idx1, idx % 5, False])
         board = Board(board_lines)
-        board_list.append(board)
+        board_list.append([board, False])
         board_lines = []
         row_counter = 0
 
-win_board = None
-winning_num = 0
-for number in numbers.split(","):
-    for board in board_list:
-        board.set_entries(int(number))
-        if board.check_win():
-            print("found")
-            win_board = board.check_win()
-            winning_num = int(number)
-    if win_board:
-        break
+# win_board = None
+# winning_num = 0
+# for number in numbers.split(","):
+#     for board in board_list:
+#         board.set_entries(int(number))
+#         if board.check_win():
+#             print("found")
+#             win_board = board.check_win()
+#             winning_num = int(number)
+#     if win_board:
+#         break
 
-winning_lines = win_board.lines
-result = sum(x[0] for x in winning_lines if not x[3])
-print(winning_num)
+# winning_lines = win_board.lines
+# result = sum(x[0] for x in winning_lines if not x[3])
+# print(winning_num)
+# print(result)
+# print(result * winning_num)
+
+last_board = None
+last_num = None
+for number in numbers.split(","):
+    if len([x for x in board_list if not x[1]]) == 1:
+        last_board = [x for x in board_list if not x[1]][0]
+        last_num = int(number)
+        last_board[0].set_entries(int(number))
+        break
+    for board in board_list:
+        board[0].set_entries(int(number))
+        if board[0].check_win():
+            print("found")
+            board[1] = True
+
+losing_lines = last_board[0].lines
+result = sum(x[0] for x in losing_lines if not x[3])
+print(last_num)
 print(result)
-print(result * winning_num)
+print(result * last_num)
